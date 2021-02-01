66fd08b398230464b7eebf000e665f2165c2ba8f
Sam Towne <sam.towne2@gmail.com>
2021-02-01 08:28:59 -0700

    Add remote s3 backend.

--- main.tf
+++ main.tf
@@ -4,6 +4,15 @@ provider "aws" {
     region = "us-east-1"
 }
 
+terraform {
+    backend "s3" {
+        bucket = "terraform-up-and-running-state-set"
+        region = "us-east-1"
+        key = "terraform.tfstate"
+        encrypt = true
+    }
+}
+
 resource "aws_launch_configuration" "example" {
     image_id = "ami-40d28157"
     instance_type = "t2.micro"
